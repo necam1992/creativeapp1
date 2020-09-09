0e34d50e8b531b1de9322b1ea1376006211ddc17
Emeka <emekannecam@gmail.com>
2020-09-09 14:46:39 -0500

    screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:creativework1/screens/startscreen.dart';
+import 'package:creativework1/screens/viewcourses.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -10,9 +11,11 @@ class CreativeWork1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: StartScreen.routeName,
       routes: {
         StartScreen.routeName: (BuildContext context) => StartScreen(),
+        ViewCourses.routeName: (context) => ViewCourses(),
       },
     );
   }
--- lib/screens/startscreen.dart
+++ lib/screens/startscreen.dart
@@ -8,24 +8,112 @@ class StartScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.red[900],
         title: Text('Student Catalog'),
       ),
       body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          children: [
-            Expanded(flex: 1, child: Image.asset('images/dbms.jpg')),
-            SizedBox(
-              height: 20.0,
-              width: 10.0,
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.fromLTRB(8.0, 10, 10, 13),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Expanded(
+                    child: FlatButton(
+                        padding: EdgeInsets.all(15),
+                        color: Colors.red[900],
+                        onPressed: () {},
+                        child: Text(
+                          'DMS',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.white),
+                        )),
+                  ),
+                  Expanded(
+                    child: FlatButton(
+                        padding: EdgeInsets.all(7),
+                        color: Colors.yellow[900],
+                        onPressed: () {},
+                        child: Text('Software Engineering',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.white))),
+                  ),
+                  Expanded(
+                    child: FlatButton(
+                        padding: EdgeInsets.all(7),
+                        color: Colors.green[500],
+                        onPressed: () {},
+                        child: Text('Mobile Application',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.white))),
+                  )
+                ],
+              ),
             ),
-            Expanded(flex: 2, child: Image.asset('images/SWE.jpg')),
-            Expanded(flex: 2, child: Image.asset('images/mobileapp.png')),
+            Divider(
+              height: 1.0,
+              color: Colors.black,
+              thickness: 1,
+              indent: 20,
+              endIndent: 20,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Container(
+                  child: RaisedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.library_books,
+                        color: Colors.green[900],
+                      ),
+                      label: Text(
+                        'CS Courses',
+                        style: TextStyle(color: Colors.green[900]),
+                      )),
+                ),
+                Container(
+                  child: RaisedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.library_books,
+                        color: Colors.green[900],
+                      ),
+                      label: Text(
+                        'CS Courses',
+                        style: TextStyle(color: Colors.green[900]),
+                      )),
+                ),
+                Container(
+                  child: RaisedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.library_books,
+                        color: Colors.green[900],
+                      ),
+                      label: Text(
+                        'CS Courses',
+                        style: TextStyle(color: Colors.green[900]),
+                      )),
+                ),
+                Container(
+                  child: RaisedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.library_books,
+                        color: Colors.green[900],
+                      ),
+                      label: Text(
+                        'CS Courses',
+                        style: TextStyle(color: Colors.green[900]),
+                      )),
+                ),
+              ],
+            )
           ],
         ),
-      )),
+      ),
     );
   }
 }
